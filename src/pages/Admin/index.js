db216168abb2edb7ba2c0c95a002b192f5e95aa2
Lorena Souza Moreira <lorenasouzamoreiraa@gmail.com>
2022-09-19 15:13:49 -0300

    criacao component table

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,10 +1,19 @@
+import { TableCell, TableHead, TableRow } from "@mui/material";
 import Header from "../../components/Header";
+import EnhancedTable from "../../components/Table";
 
 function Admin() {
+    const data = require("../../db.json");
+    console.log(data);
+
     return (
         <div className="app">
             <Header />
             <h1> admin</h1>
+            
+            <h1> Jogadores</h1>
+            <p>{data.jogadores[0].nome}</p>
+            <EnhancedTable />
         </div>
     )
 }
